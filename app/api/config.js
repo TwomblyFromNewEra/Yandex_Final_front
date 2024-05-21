8a90f5c59d97812a96d3c043235082d813bdc408
TwomblyFromNewEra <113703619+TwomblyFromNewEra@users.noreply.github.com>
2024-05-21 14:45:22 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -3,5 +3,5 @@ export const BASE_URL = 'https://lainv3pindie.nomoredomainswork.ru/api'
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}me`,
+  me: `${BASE_URL}/me`,
 }
